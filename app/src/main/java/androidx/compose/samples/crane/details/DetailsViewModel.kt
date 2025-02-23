647c5f9bd545565d19d5130850ddccd56423b945
Rafael Herdani <147459978+RafaelHerdani@users.noreply.github.com>
2025-02-23 19:29:34 +0700

    produceState

--- app/src/main/java/androidx/compose/samples/crane/details/DetailsViewModel.kt
+++ app/src/main/java/androidx/compose/samples/crane/details/DetailsViewModel.kt
@@ -22,6 +22,7 @@ import androidx.compose.samples.crane.data.ExploreModel
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
